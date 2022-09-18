29d74cb01e5b3fdfc8c5ff5c262f5538a91f8320
m <sosleepnya@gmail.com>
2022-09-18 13:43:30 -0400

    main-logic-UwU

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1 as number);
+  const possibleLocations = useRef(new Set() as Set<number>);
+  const [catMovements, setCatMovements] = useState([] as [number, number][]);
+
+  //Boxes array
+  let boxes = new Array(count).fill(0);
+
+  //We need to generate a cat in each box
+  useEffect(() => {
+    possibleLocations.current = new Set(boxes.map((_, i) => i));
+  }, [count]);
+
+  //Logic for adjustment boxes of cat movements
+  const catSteps = () => {
+    console.log("possibleLocations", possibleLocations.current);
+    let newPossibleLocations = new Set() as Set<number>;
+    let newCatMovements = [] as [number, number][];
+    possibleLocations.current.forEach((location) => {
+      let possibleSteps = [] as number[];
+      if (location - 1 >= 0) possibleSteps.push(location - 1);
+      if (location + 1 < count) possibleSteps.push(location + 1);
+      possibleSteps.forEach((step) => {
+        newPossibleLocations.add(step);
+        newCatMovements.push([location, step]);
+      });
+    });
+    console.log("newPossibleLocations", newPossibleLocations);
+    possibleLocations.current = newPossibleLocations;
+    setCatMovements(newCatMovements);
+  };
+
+  const handleBox = (index: number) => {
+    possibleLocations.current.delete(index);
+    catSteps();
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-full h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex flex-col justify-center items-center font-mono">
+      <h1 className="text-6xl">Try to catch a cat</h1>
+      <div className="flex gap-24 flex-row mt-16 ">
+        <button
+          className="text-4xl w-40 text-center"
+          disabled={count <= 1}
+          onClick={() => setCount(count - 1)}
+        >
+          Remove
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p className="text-3xl w-40 text-center">{count}</p>
+        <button
+          className="text-4xl w-40 text-center"
+          onClick={() => setCount(count + 1)}
+        >
+          Add
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex gap-3 flex-row mt-32">
+        {boxes.map((_, i) => (
+          <div>
+            <button className="text-2xl" key={i} onClick={() => handleBox(i)}>
+            <img src="box-test.svg" className="w-12 h-12"/>{i + 1}
+            </button>
+            
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
